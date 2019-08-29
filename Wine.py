2d0ab8a6598568836b1c31ba924f440958afcf96
HyowonAn <hyowon@hanwha.com>
2019-08-29 14:43:54 +0900

    Update Wine.py Model

--- Wine.py
+++ Wine.py
@@ -2,6 +2,7 @@
 from keras.models import Sequential, load_model
 from keras.layers.core import Dense
 from keras.callbacks import ModelCheckpoint, EarlyStopping
+from sklearn.model_selection import train_test_split
 import pandas as pd
 import numpy as np
 import tensorflow as tf
@@ -11,21 +12,23 @@ import os
 seed = 201908
 np.random.seed(seed)
 tf.set_random_seed(seed)
-model = None
 
 #%%
 df_pre = pd.read_csv('dataset/Wine.csv', header=None, names=["주석산농도", "아세트산농도", "구현산농도", "잔류 당분 농도",
                                                              "염화나트륨 농도", "유리 아황산 농도", " 총 아황산 농도", "밀도", "pH", "황산칼륨 농도", "알콜 도수", "와인맛(0~10)", "Class"])
 df_pre.info()
 
-df = df_pre.sample(frac=0.15)
+df = df_pre.sample(frac=0.3)
 df.info()
 #%%
 dataset = df.values
 column_size = dataset.shape[1]-1
 X = dataset[:, 0:column_size]
 Y = dataset[:, column_size]
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=seed)
+
 #%%
+model = None
 model = Sequential()
 model.add(Dense(30, input_dim=column_size, activation='relu'))
 model.add(Dense(12, activation='relu'))
@@ -49,7 +52,7 @@ checkpointer_callback = ModelCheckpoint(
     filepath=modelpath, monitor='val_loss', verbose=1, save_best_only=True)
 early_stopping_callback = EarlyStopping(monitor='val_loss', patience=100) #if model doesn't improve next prev 100 times, then stop
 #%% 
-history = model.fit(X, Y, validation_split=0.33, epochs=3500,
+history = model.fit(X_train, Y_train, validation_split=0.33, epochs=3500,
                     batch_size=500, verbose=False, callbacks=[checkpointer_callback, early_stopping_callback])
 
 #%% 
@@ -67,6 +70,6 @@ if model is not None:
 model = load_model(modelpath)
 
 #%%
-print('Loaded Model\'s Accuracy: %.4f' %(model.evaluate(X,Y)[1]))
+print('Loaded Model\'s Accuracy: %.4f' %(model.evaluate(X_test,Y_test)[1]))
 
 #%%
